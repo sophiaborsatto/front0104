446b7a414406a63091bb5162159b8427d07030b9
sophiaborsatto <sophia-borsatto@hotmail.com>
2025-04-01 20:40:27 -0300

    aula0104
    Calculadora funcional

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function expressao(parametro){
+     document.getElementById('tela').value += parametro;
+}
+
+function resultado(){
+
+    try {
+        let expressao = document.getElementById('tela').value;
+        let resultado = eval(expressao);
+        document.getElementById('tela').value = resultado;
+    } catch (error) {
+        document.getElementById('tela').value = "Error";
+    }
+}
+
+function limpar(){
+
+}
